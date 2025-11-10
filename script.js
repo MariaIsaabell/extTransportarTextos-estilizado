561d131c9a1511a7ae602d56f6cc7cdb4c4af692
MariaIsaabell <pmariaisabel229@gmail.com>
2025-11-10 12:16:51 -0300

    Primeira Versão - extensão

---
+++ script.js
@@ -0,0 +1,41 @@
+/* Programa - Modelo 1 */
+
+window.document.addEventListener("DOMContentLoaded", function () {
+  /* Botão Letra Maiúscula */
+  window.document
+    .getElementById("btnMaiuscula")
+    .addEventListener("click", function () {
+      let inputText = document.querySelector("#input-text").value;
+      document.querySelector("#result").innerHTML = inputText.toUpperCase();
+    });
+  /* Botão Letra Maiúscula */
+  window.document
+    .getElementById("btnMinuscula")
+    .addEventListener("click", function () {
+      let inputText = document.querySelector("#input-text").value;
+      document.querySelector("#result").innerHTML = inputText.toLowerCase();
+    });
+
+  window.document
+    .getElementById("btnPrimeiraLetraMaiuscula")
+    .addEventListener("click", function () {
+      let inputText = document.querySelector("#input-text").value;
+      let resultado = inputText.charAt(0).toUpperCase() + inputText.slice(1).toLowerCase();
+      document.querySelector("#result").innerHTML = resultado;
+    });
+
+  window.document
+    .getElementById("btnLimpar")
+    .addEventListener("click", function () {
+      document.querySelector("#input-text").value = " ";
+      document.querySelector("#result").innerHTML = " ";
+    });
+
+  window.document
+    .getElementById("btnNegrito")
+    .addEventListener("click", function () {
+      let inputText = document.querySelector("#input-text").value;
+      let resultado = `<strong>${inputText}</strong>`;
+      document.querySelector("#result").innerHTML = resultado;
+    });
+});
